9046c6b7f00fab1d68649ff4d7a3e854b6147b81
thijsbrits <thijsbrits@gmail.com>
2024-09-29 18:16:07 +0200

    use executemany

--- main.py
+++ main.py
@@ -17,28 +17,39 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 def populate_electric_vehicles_from_csv(conn, csv_path):
-    """Populate electricvehicles table from CSV file."""
+    """Populate the electricvehicles table from a CSV file."""
 
     table_name = "electricvehicles"
     field_names = ElectricVehicle.field_names()
     columns_str = ", ".join(field_names)
     placeholders_str = create_placeholder_str(field_names)
-    insert_query = f'''INSERT INTO {table_name} ({columns_str}) VALUES ({placeholders_str})'''
+    insert_query = f"INSERT INTO {table_name} ({columns_str}) VALUES ({placeholders_str})"
 
-    total_records_to_be_inserted = 0
+    logger.info("Starting insertion into electricvehicles from CSV file.")
 
-    logger.info("Starting electricvehicles insertion from CSV file.")
-
-    conn.execute("BEGIN TRANSACTION")
+    # Collect all records to be inserted
+    records_to_insert = []
     for row in load_csv_rows(csv_path):
         electric_vehicle = ElectricVehicle.from_dict(row)
-        total_records_to_be_inserted += 1
-        conn.execute(insert_query, astuple(electric_vehicle))
-    conn.execute("COMMIT")
+        records_to_insert.append(astuple(electric_vehicle))
 
-    assert total_records_to_be_inserted == conn.execute('SELECT count(*) FROM electricvehicles').fetchone()[0]
+    total_records = len(records_to_insert)
 
-    logger.info(f"Insertion successful, inserted records: {total_records_to_be_inserted}")
+    try:
+        conn.executemany(insert_query, records_to_insert)
+
+        # Verify the number of records inserted
+        db_record_count = conn.execute('SELECT COUNT(*) FROM electricvehicles').fetchone()[0]
+        assert total_records == db_record_count, (
+            f"Mismatch in record counts: expected {total_records}, "
+            f"found {db_record_count}"
+        )
+
+        logger.info(f"Insertion successful. Total records inserted: {total_records}")
+    except Exception as e:
+        logger.error(f"An error occurred during insertion: {e}")
+        raise
+
 
 
 def main(csv_path, ddl_path, db_path):
